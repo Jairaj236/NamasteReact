9c71bfabdb196b527ce6eff06416cb2b63e180f0
Jaya Raju <svara.jayaraju.actualpixel.in>
2025-04-20 14:13:40 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,3 @@
+const heading = React.createElement('div',{id:"parent"},React.createElement('h1',{},"this heading from the react"))
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading)
